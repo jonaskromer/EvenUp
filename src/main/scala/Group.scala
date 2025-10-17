5673b613cd518028fad793f4f5f39f4dd4ac2a4d
JONAS KROMER <jonas.kromer@dlh.de>
2025-10-17 10:54:26 +0200

    fix: adjust naming

--- src/main/scala/Group.scala
+++ src/main/scala/Group.scala
@@ -2,10 +2,10 @@ final case class Group(name: String, members: List[Person], expenses: List[Expen
 
   override def toString(): String = 
 
-    val membersString = members.map(_.toString()).mkString(", ")
-    val expensesString = expenses.map(_.toString()).mkString("\n")
+    val members_string = members.map(_.toString()).mkString(", ")
+    val expenses_string = expenses.map(_.toString()).mkString("\n")
 
-    f"The group $name has the following users and expenses.\nUsers: $membersString\nExpenses:\n$expensesString"
+    f"The group $name has the following users and expenses.\nUsers: $members_string\nExpenses:\n$expenses_string"
 
   def addMember(person: Person): Group =
     if (members.contains(person)) this else copy(members = members :+ person)

71b06c48e264c7ccb1ce794d2b437e0f22d7f007
jonaskromer <jonas.kromer27@gmail.com>
2025-10-18 17:46:33 +0200

    feat: add update methods

--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -1,3 +1,6 @@
-final case class Person(name: String):
-    
+final case class Person(name: String): //future: mail, friendlist, login, picture
+
     override def toString(): String = f"$name"
+
+    def updateName(name: String): Person =
+        copy(name = name)
--- src/main/scala/Transaction.scala
+++ src/main/scala/Transaction.scala
@@ -2,3 +2,14 @@ final case class Transaction(from: Person, to: Person, amount: Double):
 
     override def toString(): String = 
         f"${from.name} paid $amount to ${to.name}."
+
+    def updateFrom(from: Person): Transaction =
+        copy(from = from)
+    
+    def updateTo(to: Person): Transaction =
+        copy(to = to)
+
+    def updateAmount(amount: Double): Transaction =
+        copy(amount = amount)
+
+        

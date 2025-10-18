be4cc8cebbff985e90d427d3c90f82f5aac5e468
jonaskromer <jonas.kromer27@gmail.com>
2025-10-18 17:34:42 +0200

    feat: add update methods

--- src/main/scala/Expense.scala
+++ src/main/scala/Expense.scala
@@ -8,3 +8,18 @@ final case class Expense(name: String, amount: Double, date: String, paid_by: Pe
             }.mkString(", ")
 
         s"${paid_by.name} paid $amount€ for $name on $date. $sharesString"
+
+    def updateName(name: String): Expense =
+        copy(name = name)
+
+    def updateAmount(amount: Double): Expense =
+        copy(amount = amount)
+
+    def updateDate(date: String): Expense =
+        copy(date = date)
+
+    def updatePaidBy(paid_by: Person): Expense =
+        copy(paid_by = paid_by)
+
+    def updateShares(shares: Map[Person, Double]): Expense =
+        copy(shares = shares)
--- src/main/scala/Group.scala
+++ src/main/scala/Group.scala
@@ -1,16 +1,23 @@
 final case class Group(name: String, members: List[Person], expenses: List[Expense]):
 
-  override def toString(): String = 
+    override def toString(): String = 
 
-    val members_string = members.map(_.toString()).mkString(", ")
-    val expenses_string = expenses.map(_.toString()).mkString("\n")
+        val members_string = members.map(_.toString()).mkString(", ")
+        val expenses_string = expenses.map(_.toString()).mkString("\n")
 
-    f"The group $name has the following users and expenses.\nUsers: $members_string\nExpenses:\n$expenses_string"
+        f"The group $name has the following users and expenses.\nUsers: $members_string\nExpenses:\n$expenses_string"
 
-  def addMember(person: Person): Group =
-    if (members.contains(person)) this else copy(members = members :+ person)
+    def addMember(person: Person): Group =
+        if (members.contains(person)) this else copy(members = members :+ person)
 
-  def addExpense(expense: Expense): Group =
-    copy(expenses = expenses :+ expense)
+    def removeMember(person: Person): Group =
+        if (members.contains(person)) copy(members = members :- person) else this
 
-    //
+    def addExpense(expense: Expense): Group =
+        copy(expenses = expenses :+ expense)
+
+    def removeExpense(expense: Expense): Group =
+        if(expenses.contains(expense)) this else copy(expenses = expenses :- expense)
+
+    def updateName(name: String): Group =
+        copy(name = name)

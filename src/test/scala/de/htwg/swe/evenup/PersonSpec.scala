b213fc773d3b503370d2b9ee7a82e2fbad9a3f88
jonaskromer <jonas.kromer27@gmail.com>
2025-10-19 10:16:26 +0200

    feat: added first test classes

---
+++ src/test/scala/de/htwg/swe/evenup/PersonSpec.scala
@@ -0,0 +1,19 @@
+package de.htwg.swe.evenup
+
+import org.scalatest._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import de.htwg.swe.evenup._
+
+class PersonSpec extends AnyWordSpec with Matchers:
+    
+    "When updating name the correct name has to be shown" in:
+        val p_1 = Person("John")
+        val new_name = "Peter"
+        val p_2 = p_1.updateName(new_name)
+        p_2.name shouldBe new_name
+    
+    "The String of a person should print as follows" in:
+        val p_1 = Person("John")
+        p_1.toString() shouldBe "John" 

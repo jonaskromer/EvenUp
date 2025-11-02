009df2e9ea0087a1de77b328f7f6a080c1c985d0
jonaskromer <jonas.kromer27@gmail.com>
2025-11-02 12:10:05 +0100

    feat: adopted main method to only use tui and controller

---
+++ src/main/scala/de/htwg/swe/evenup/EvenUp.scala
@@ -0,0 +1,19 @@
+package de.htwg.swe.evenup
+
+import de.htwg.swe.evenup.model.App
+import de.htwg.swe.evenup.control.Controller
+
+import scala.io.StdIn.readLine
+import de.htwg.swe.evenup.view.Tui
+
+@main def hello(): Unit =
+
+  val app        = App(Nil)
+  val controller = new Controller(app)
+  val tui        = new Tui(controller)
+
+  var input = ""
+
+  while (input != "exit")
+    input = readLine()
+    tui.processInput(input)

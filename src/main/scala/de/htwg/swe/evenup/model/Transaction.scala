7dd892915352d9f2cadf80b0668e45baa066ae24
jonaskromer <jonas.kromer27@gmail.com>
2025-11-08 22:50:59 +0100

    fix: formatting

--- src/main/scala/de/htwg/swe/evenup/model/Transaction.scala
+++ src/main/scala/de/htwg/swe/evenup/model/Transaction.scala
@@ -1,14 +1,22 @@
 package de.htwg.swe.evenup.model
 
 import de.htwg.swe.evenup.model.Person
+import de.htwg.swe.evenup.model.Date
 
-final case class Transaction(from: Person, to: Person, amount: Double):
+final case class Transaction(
+  from: Person,
+  to: Person,
+  amount: Double,
+  date: Date
+):
 
   override def toString(): String =
-    f"${from.name} paid $amount%.2f to ${to.name}."
+    f"${from.name} paid $amount%.2f to ${to.name} on ${date}."
 
   def updateFrom(from: Person): Transaction = copy(from = from)
 
   def updateTo(to: Person): Transaction = copy(to = to)
 
   def updateAmount(amount: Double): Transaction = copy(amount = amount)
+
+  def updateDate(date: Date): Transaction = copy(date = date)

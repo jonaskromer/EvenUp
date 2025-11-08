bdeb7e43d16447b699ff9ebf3f1ae2345825c835
jonaskromer <jonas.kromer27@gmail.com>
2025-11-08 22:49:37 +0100

    feat: added active user and group to track in tui

--- src/main/scala/de/htwg/swe/evenup/model/App.scala
+++ src/main/scala/de/htwg/swe/evenup/model/App.scala
@@ -3,7 +3,11 @@ package de.htwg.swe.evenup.model
 import de.htwg.swe.evenup.model.Group
 import de.htwg.swe.evenup.model.Person
 
-final case class App(groups: List[Group]):
+final case class App(
+  groups: List[Group],
+  active_user: Option[String],
+  active_group: Option[String]
+):
 
   def allGroups: List[Group] = groups
 
@@ -13,6 +17,10 @@ final case class App(groups: List[Group]):
 
   def updateGroup(updatedGroup: Group): App = copy(groups =
     groups.map(g => if g.name == updatedGroup.name then updatedGroup else g)
+  )
+
+  def updateActiveGroup(active_group: Option[String]): App = copy(active_group =
+    active_group
   )
 
   def findGroupByName(name: String): Option[Group] = groups.find(_.name == name)

7d0ca1b74114cc0fb49264d4e27942521788883c
jonaskromer <jonas.kromer27@gmail.com>
2025-11-08 22:51:41 +0100

    feat: added observer events and more base methods

--- src/main/scala/de/htwg/swe/evenup/control/Controller.scala
+++ src/main/scala/de/htwg/swe/evenup/control/Controller.scala
@@ -1,21 +1,43 @@
 package de.htwg.swe.evenup.control
 
 import de.htwg.swe.evenup.model.App
-import de.htwg.swe.evenup.util.Observable
+import de.htwg.swe.evenup.util.{Observable, ObservableEvent}
 import de.htwg.swe.evenup.model.Group
 import de.htwg.swe.evenup.model.Person
 import de.htwg.swe.evenup.model.Expense
+import de.htwg.swe.evenup.model.Date
+
+enum ControllerEvent extends ObservableEvent:
+  case Quit
+  case MainMenu
+  case NewGroupCreated
+  case PersonAddedToGroup
+  case ExpenseAddedToGroup
+  case GroupCreationFinished
 
 class Controller(var app: App) extends Observable {
+
+  def quit: Unit =
+    // TODO: save state
+    notifyObservers(ControllerEvent.Quit)
 
   def allGroups: List[Group] = app.groups
 
   def allUsers: List[Person] = app.groups.flatMap(_.members).distinct
 
+  def finishGroupSetup: Unit = notifyObservers(
+    ControllerEvent.GroupCreationFinished
+  )
+
+  def gotoMainMenu: Unit =
+    app.updateActiveGroup(None)
+    notifyObservers(ControllerEvent.MainMenu)
+
   def addGroup(name: String): Unit =
     val newGroup = Group(name, Nil, Nil)
     app = app.addGroup(newGroup)
-    notifyObservers
+    app = app.updateActiveGroup(Some(name))
+    notifyObservers(ControllerEvent.NewGroupCreated)
 
   def addPersonToGroup(group_name: String, person_name: String): Unit =
     app.findGroupByName(group_name) match
@@ -23,28 +45,23 @@ class Controller(var app: App) extends Observable {
         val newPerson    = Person(person_name)
         val updatedGroup = group.addMember(newPerson)
         app = app.updateGroup(updatedGroup)
-        notifyObservers
-      case None =>
-        throw new NoSuchElementException(
-          s"Group '$group_name' not found."
-        ) // TODO: fix error handling to scala style
+        notifyObservers(ControllerEvent.PersonAddedToGroup)
+      case None => println("Group not found.")
 
   def addExpenseToGroup(
     group_name: String,
     expense_name: String,
     paid_by: Person,
     amount: Double,
+    date: Date,
     shares: Map[Person, Double]
   ): Unit =
     app.findGroupByName(group_name) match
       case Some(group) =>
-        val newExpense   = Expense(expense_name, amount, "0", paid_by, shares)
+        val newExpense   = Expense(expense_name, amount, date, paid_by, shares)
         val updatedGroup = group.addExpense(newExpense)
         app = app.updateGroup(updatedGroup)
-        notifyObservers
-      case None =>
-        throw new NoSuchElementException(
-          s"Group '$group_name' not found."
-        ) // TODO: fix error handling to scala style
+        notifyObservers(ControllerEvent.ExpenseAddedToGroup)
+      case None => println("Group not found.")
 
 }

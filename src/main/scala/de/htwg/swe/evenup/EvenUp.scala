4ff4cac7e3805ca0e09cff6b06a8844ab2c605f7
jonaskromer <jonas.kromer27@gmail.com>
2025-11-09 15:03:47 +0100

    fix: named main method main

--- src/main/scala/de/htwg/swe/evenup/EvenUp.scala
+++ src/main/scala/de/htwg/swe/evenup/EvenUp.scala
@@ -6,7 +6,7 @@ import de.htwg.swe.evenup.control.Controller
 import scala.io.StdIn.readLine
 import de.htwg.swe.evenup.view.tui.Tui
 
-@main def hello(): Unit =
+@main def main(): Unit =
 
   val app        = App(Nil, None, None)
   val controller = new Controller(app)

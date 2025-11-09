438f3de01508634d82a3011615d807785a487b0b
jonaskromer <jonas.kromer27@gmail.com>
2025-11-09 15:04:30 +0100

    feat: added description and usage information

--- src/main/scala/de/htwg/swe/evenup/view/tui/TuiKeys.scala
+++ src/main/scala/de/htwg/swe/evenup/view/tui/TuiKeys.scala
@@ -1,12 +1,14 @@
 package de.htwg.swe.evenup.view.tui
 
-enum TuiKeys(val key: String):
-  case newGroup       extends TuiKeys(":ng")
-  case newPerson      extends TuiKeys(":np")
-  case newExpense     extends TuiKeys(":ne")
-  case newTransaction extends TuiKeys(":nt")
-  case addUserToGroup extends TuiKeys(":aug")
-  case help           extends TuiKeys(":h")
-  case quit           extends TuiKeys(":q")
-  case proceed        extends TuiKeys(":p")
-  case MainMenu       extends TuiKeys(":m")
+enum TuiKeys(val key: String, val usage: String, val description: String):
+  case newGroup        extends TuiKeys(":newgroup", "<group name>", "Add a group")
+  case gotoGroup       extends TuiKeys(":group", "<group name>", "Open a group")
+  case addExpense      extends TuiKeys(":addexp", "<name> <amount> <date> <paid_by> <opt:shares>", "Add an expense")
+  case editExpense     extends TuiKeys(":editexp", "tbd", "Edit an expense")
+  case newTransaction  extends TuiKeys(":pay", "tbd", "Add a new transaction")
+  case editTransaction extends TuiKeys(":editpay", "tbd", "Edit a transaction")
+  case addUserToGroup  extends TuiKeys(":adduser", "<user name> <user name> ...", "Add a user to a group")
+  case help            extends TuiKeys(":h", "", "Help")
+  case quit            extends TuiKeys(":q", "", "Quit")
+  case proceed         extends TuiKeys(":p", "", "Proceed")
+  case MainMenu        extends TuiKeys(":m", "", "Go into the main menu")

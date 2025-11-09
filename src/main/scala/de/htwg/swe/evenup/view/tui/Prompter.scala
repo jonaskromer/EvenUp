991fe70d5f511adc83a9e35348c0bcbc5e9255e1
jonaskromer <jonas.kromer27@gmail.com>
2025-11-09 15:04:59 +0100

    feat: added ">" in line of input

--- src/main/scala/de/htwg/swe/evenup/view/tui/Prompter.scala
+++ src/main/scala/de/htwg/swe/evenup/view/tui/Prompter.scala
@@ -2,22 +2,34 @@ package de.htwg.swe.evenup.view.tui
 
 class Prompter {
 
-  def promptNewGroup: Unit = println(
-    f"Create a new group by using => ${TuiKeys.newGroup.key} <group name>"
-  )
+  def promptNewGroup: Unit =
+    println(
+      f"Create a new group by using => ${TuiKeys.newGroup.key} <group name>"
+    )
+    print(">")
 
-  def promptAddUser: Unit = println(
-    f"Add a user to a group by using => ${TuiKeys.addUserToGroup.key} <group name> <user name>"
-  )
+  def promptAddUser: Unit =
+    println(
+      f"Add a user to a group by using => ${TuiKeys.addUserToGroup.key} <group name> <user name>"
+    )
+    print(">")
 
-  def promptAddUserOrContinue: Unit = println(
-    f"Add a user to a group by using => ${TuiKeys.addUserToGroup.key} <group name> <user name> or proceed by using => ${TuiKeys.proceed.key}"
-  )
+  def promptAddUserOrContinue: Unit =
+    println(
+      f"Add a user to a group by using => ${TuiKeys.addUserToGroup.key} <group name> <user name> or proceed by using => ${TuiKeys.proceed.key}"
+    )
+    print(">")
 
-  def promptInGroup: Unit = println(
-    f"Add Expense by....."
-  )
+  def promptInGroup: Unit =
+    println(f"Add Expense by.....")
+    print(">")
 
-  def promptMainMenu: Unit = println(f"Goto group by....")
+  def promptMainMenu: Unit =
+    println(f"Goto group by....")
+    print(">")
+
+  def promptInitGroup: Unit =
+    println(s"This group is only initialized. add members or expenses...")
+    print(">")
 
 }

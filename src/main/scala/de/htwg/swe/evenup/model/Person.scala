18d53beebc113c5a302dc9abd42070feb3b77cfc
jonaskromer <jonas.kromer27@gmail.com>
2025-11-13 18:47:02 +0100

    feat: added equals and hash method

--- src/main/scala/de/htwg/swe/evenup/model/Person.scala
+++ src/main/scala/de/htwg/swe/evenup/model/Person.scala
@@ -5,5 +5,11 @@ final case class Person(
 ): // future: mail, friendlist, login, picture
 
   override def toString(): String = f"$name"
+  
+  override def equals(obj: Any): Boolean = obj match
+    case p: Person => p.name == this.name
+    case _ => false
+
+  override def hashCode(): Int = name.hashCode
 
   def updateName(name: String): Person = copy(name = name)

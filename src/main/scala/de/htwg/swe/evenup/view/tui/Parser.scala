8f49e873ac9a2dcfffe081351fba18642bed345f
jonaskromer <jonas.kromer27@gmail.com>
2025-11-13 21:21:45 +0100

    feat: further dev

--- src/main/scala/de/htwg/swe/evenup/view/tui/Parser.scala
+++ src/main/scala/de/htwg/swe/evenup/view/tui/Parser.scala
@@ -3,6 +3,8 @@ package de.htwg.swe.evenup.view.tui
 import scala.util.Try
 import scala.util.Success
 import scala.util.Failure
+import de.htwg.swe.evenup.model.Share
+import de.htwg.swe.evenup.model.Person
 
 class Parser {
 
@@ -15,5 +17,21 @@ class Parser {
         println("Wrong Usage")
         None
   }
+
+  def parseShares(input: Option[String]): Option[List[Share]] =
+    def getShares(input: String) = input.split("_").toList.map { s =>
+      s.split(":") match
+        case Array(name, amount) => Share(Person(name), amount.toDouble)
+        case _ => throw new IllegalArgumentException(s"Invalid share: $s")
+    }
+    input match
+      case Some(in) =>
+        val tryParse = Try(getShares(in))
+        tryParse match
+          case Success(shares)    => Some(shares)
+          case Failure(exception) =>
+            println("Wrong Usage of shares")
+            None
+      case None => None
 
 }
--- src/main/scala/de/htwg/swe/evenup/view/tui/Tui.scala
+++ src/main/scala/de/htwg/swe/evenup/view/tui/Tui.scala
@@ -1,11 +1,10 @@
 package de.htwg.swe.evenup.view.tui
 
-import de.htwg.swe.evenup.control.{Controller, ControllerEvent}
 import de.htwg.swe.evenup.util.{ObservableEvent, Observer}
-
-import scala.io.StdIn.readLine
 import de.htwg.swe.evenup.model.Person
 import de.htwg.swe.evenup.model.Group
+import de.htwg.swe.evenup.model.Share
+import de.htwg.swe.evenup.control.*
 
 enum PromptState {
   case None
@@ -25,11 +24,17 @@ class Tui(controller: Controller) extends Observer {
 
   def printHelp: Unit =
     val maxDescriptionLen = TuiKeys.values.map(_.description.length).max
-    val maxKeyLen  = TuiKeys.values.map(_.key.length).max
+    val maxKeyLen         = TuiKeys.values.map(_.key.length).max
 
     for key <- TuiKeys.values do
-      println(String.format(s"%-${maxDescriptionLen}s ==> %-${maxKeyLen}s %s",
-        key.description, key.key, key.usage))
+      println(
+        String.format(
+          s"%-${maxDescriptionLen}s ==> %-${maxKeyLen}s %s",
+          key.description,
+          key.key,
+          key.usage
+        )
+      )
 
   def printFullOverview: Unit =
     println("_" * 40)
@@ -49,57 +54,76 @@ class Tui(controller: Controller) extends Observer {
 
   println("Welcome to EvenUp!")
 
-  /*
-  val user = readLine("Please enter your name: ")
-  println(f"Logged in as $user.")
-   */
   println(
     f"Start by adding a group with => ${TuiKeys.newGroup.key} <group name>"
   )
 
+  print(">")
+
+  val addGroupHandler: PartialFunction[ControllerEvent, Unit] =
+    case ControllerEvent.AddGroup(AddGroupResult.Success, group) =>
+      println(f"Added group ${group.name}")
+
+  val gotoGroupHandler: PartialFunction[ControllerEvent, Unit] = 
+    case ControllerEvent.GotoGroup(GotoGroupResult.Success, group) =>
+      println(s"Set active group to ${group.name}")
+    case ControllerEvent.GotoGroup(GotoGroupResult.GroupNotFound, group) =>
+      println(s"Unable to find group ${group.name}")
+
+  val addUserToGroupHandler: PartialFunction[ControllerEvent, Unit] =
+    case ControllerEvent.AddUserToGroup(AddUserToGroupResult.Success, user) =>
+      println(f"Added ${user.name} to ${controller.app.active_group.get.name}.") // IS THIS VALID OR SHOULD I RETURN ALSO A GROUP???
+    case ControllerEvent.AddUserToGroup(AddUserToGroupResult.UserAlreadyAdded, user) => 
+      println(f"User ${user.name} already added to group ${controller.app.active_group.get.name}!")
+    case ControllerEvent.AddUserToGroup(AddUserToGroupResult.NoActiveGroup, user) =>
+      println(f"Cannot add ${user.name} because there is no active group!")
+
+  val expenseHandler: PartialFunction[ControllerEvent, Unit] = 
+    case ControllerEvent.AddExpense(AddExpenseResult.Success, expense) =>
+      println(s"Added expense ${expense}")
+    case ControllerEvent.AddExpense(AddExpenseResult.ActiveGroupNotFound, expense) => 
+      println("Please first goto a group.")
+    case ControllerEvent.AddExpense(AddExpenseResult.SharesSumWrong, expense) =>
+      println("The sum of the shares do not match with the sum of the expense.")
+    case ControllerEvent.AddExpense(AddExpenseResult.SharesPersonNotFound, expense) =>
+      println(s"Wrong user in shares.")
+    case ControllerEvent.AddExpense(AddExpenseResult.PaidByNotFound, expense) =>
+      println(s"Please first add ${expense.paid_by} to the group before using in expense.")
+
+  val handler: PartialFunction[ControllerEvent, Unit] =
+    addGroupHandler orElse gotoGroupHandler orElse expenseHandler orElse addUserToGroupHandler
+
   override def update(event: ObservableEvent): Unit =
     event match
-      case ControllerEvent.Quit =>
-        println("Goodbye!")
-        System.exit(0)
-
-      case ControllerEvent.MainMenu =>
-        promptState = PromptState.MainMenu
-        printAvailableGroups
-        prompter.promptMainMenu
-
-      case ControllerEvent.GroupInitialized =>
-        promptState = PromptState.InGroup
+      case e: ControllerEvent if handler.isDefinedAt(e) => 
+        handler(e)
         printActiveGroup
-        prompter.promptInitGroup
-
-      case ControllerEvent.InGroup =>
-        promptState = PromptState.InGroup
-        printActiveGroup
-        prompter.promptInGroup
-
-      case ControllerEvent.PersonAddedToGroup =>
-        // no real use
-        promptState = PromptState.InGroup
-        printFullOverview
-        prompter.promptInGroup
-
-      case ControllerEvent.ExpenseAddedToGroup =>
-        promptState = PromptState.InGroup
-        printFullOverview
-        prompter.promptInGroup
+      case ControllerEvent.MainMenu => printFullOverview
+      case ControllerEvent.Quit => println("GoodBye!")
+      case _ => println("Unhandled event...")
 
   def processInput(input: String): Unit =
     val in = input.split(" ").toList
     in.head match
       case TuiKeys.help.key     => printHelp
       case TuiKeys.quit.key     => controller.quit
-      case TuiKeys.newGroup.key => controller.addGroup(Group(in.drop(1).mkString(" "), Nil, Nil))
-      case TuiKeys.addUserToGroup.key => in.drop(1).foreach(person_name => controller.addPersonToGroup(Person(person_name)))
-      case TuiKeys.addExpense.key => controller.addExpenseToGroup(in(1), in(2), in(3).toDouble)
+      case TuiKeys.newGroup.key =>
+        controller.addGroup(Group(in.drop(1).mkString(" "), Nil, Nil, Nil))
+      case TuiKeys.addUserToGroup.key =>
+        in.drop(1)
+          .foreach(person_name =>
+            controller.addUserToGroup(Person(person_name))
+          )
+      case TuiKeys.addExpense.key =>
+        controller.addExpenseToGroup(
+          in(1),
+          Person(in(2)),
+          in(3).toDouble,
+          shares = parser.parseShares(in.lift(4))
+        )
       case TuiKeys.MainMenu.key  => controller.gotoMainMenu
-      case TuiKeys.gotoGroup.key => controller.gotoGroup(Group(in.drop(1).mkString(" "), Nil, Nil))
-      case _ => 
-        println("This key is not supported... yet :)")
-        print(">")
+      case TuiKeys.gotoGroup.key =>
+        controller.gotoGroup(Group(in.drop(1).mkString(" "), Nil, Nil, Nil))
+      case _ => println("This key is not supported... yet :)")
+
 }

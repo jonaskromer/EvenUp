b84ec231b2e7b72d9d6cd2abc3108f7a3fb3cce2
jonaskromer <jonas.kromer27@gmail.com>
2025-12-04 20:25:46 +0100

    feat: implemented Parser for string input

--- src/main/scala/de/htwg/swe/evenup/view/tui/Tui.scala
+++ src/main/scala/de/htwg/swe/evenup/view/tui/Tui.scala
@@ -9,23 +9,25 @@ import de.htwg.swe.evenup.model.financial.debt.NormalDebtStrategy
 import de.htwg.swe.evenup.model.state.AppState
 import de.htwg.swe.evenup.model.Date
 
-enum PromptState {
-  case None
-  case GetOrCreateGroup
-  case InGroup
-  case SetupExpense
-  case MainMenu
-}
+import scala.util.{Try, Success, Failure}
 
-class Tui(controller: Controller) extends Observer {
-  var promptState: PromptState = PromptState.None
-
-  val parser   = new Parser
-  val prompter = new Prompter
-
+class Tui(controller: Controller) extends Observer:
+  
   controller.add(this)
 
-  def spacer = "_" * 40
+  val parser           = new Parser
+  val tuiStringBuilder = new TuiStringBuilder(controller)
+
+  val welcome_message= new ColorDecorator(
+          new BorderDecorator(
+            new TextComponent(s"""|Welcome to EvenUp!
+                                  |Start by adding a group with => ${TuiKeys.newGroup.key} <group name>""".stripMargin),
+            "#"
+          ),
+          ConsoleColors.BRIGHT_BLUE
+        ).render
+
+  print(welcome_message + "\n>")
 
   def printHelp(state: AppState): Unit =
     val keys              = TuiKeys.values.filter(_.allowed(state))
@@ -58,60 +60,6 @@ class Tui(controller: Controller) extends Observer {
 
     println(decoratedHelp.render)
 
-  def buildFullOverviewString: String = Seq(
-    spacer,
-    controller.app.allGroups.map(_.toString).mkString("\n"),
-    spacer
-  ).mkString("\n")
-
-  def getAvailableGroupsString: String = Seq(
-    spacer,
-    "Available Groups:",
-    spacer,
-    controller.app.allGroups.map(_.name).mkString("\n"),
-    spacer
-  ).mkString("\n")
-
-  def getActiveGroupString: String =
-    controller.app.active_group match
-      case Some(group) =>
-        val decoratedHeader =
-          new ColorDecorator(
-            new TextComponent(s"Active Group: ${group.name}"),
-            ConsoleColors.BRIGHT_CYAN
-          )
-        val decoratedGroup =
-          new HeaderFooterDecorator(
-            new ColorDecorator(
-              new BorderDecorator(
-                new TextComponent(group.toString)
-              ),
-              ConsoleColors.BRIGHT_GREEN
-            ),
-            header = Some(decoratedHeader.render)
-          )
-        decoratedGroup.render
-      case None => ""
-
-  val welcomeText = "Welcome to EvenUp!"
-  val instructionText = s"Start by adding a group with => ${TuiKeys.newGroup.key} <group name>"
-
-  val decoratedWelcome =
-    new HeaderFooterDecorator(
-      new ColorDecorator(
-        new BorderDecorator(
-          new TextComponent(welcomeText)
-        ),
-        ConsoleColors.BRIGHT_GREEN
-      ),
-      footer = Some(instructionText)
-    )
-
-  println(decoratedWelcome.render)
-  print(">")
-
-  val tuiStringBuilder = new TuiStringBuilder(controller)
-
   override def update(event: ObservableEvent): Unit =
     event match
       case e: EventResponse if tuiStringBuilder.isDefined(e) =>
@@ -119,7 +67,6 @@ class Tui(controller: Controller) extends Observer {
           Seq(
             "\n",
             tuiStringBuilder.handle(e),
-            getActiveGroupString,
             ">"
           ).mkString("\n")
         )
@@ -127,29 +74,29 @@ class Tui(controller: Controller) extends Observer {
       case _                                 => println(s"Unhandled event...\n${event}")
 
   def processInput(input: String): Unit =
-    val in = input.split(" ").toList
-    in.head match
-      case TuiKeys.help.key           => printHelp(controller.app.state)
-      case TuiKeys.quit.key           => controller.quit
-      case TuiKeys.undo.key           => controller.undo()
-      case TuiKeys.redo.key           => controller.redo()
-      case TuiKeys.newGroup.key       => controller.addGroup(in(1))
-      case TuiKeys.addUserToGroup.key =>
-        in.drop(1)
-          .foreach(user_name => controller.addUserToGroup(user_name))
-      case TuiKeys.addExpense.key =>
-        controller.addExpenseToGroup(
-          in(1),
-          in(2),
-          in(3).toDouble,
-          date = Date(1, 1, 2000), // TODO: fix this
-          shares = in.lift(4)
-        )
-      case TuiKeys.MainMenu.key       => controller.gotoMainMenu
-      case TuiKeys.gotoGroup.key      => controller.gotoGroup(in(1))
-      case TuiKeys.calculateDebts.key => controller.calculateDebts()
-      case TuiKeys.setStrategy.key    => controller.setDebtStrategy(in.drop(1).mkString(" "))
-      case _                          =>
-        println("This key is not supported... yet :)")
-        print(">")
-}
+    parser.parseInput(input) match
+      case Success(tokens) =>
+        tokens.head match
+          case TuiKeys.help.key           => printHelp(controller.app.state)
+          case TuiKeys.MainMenu.key       => controller.gotoMainMenu
+          case TuiKeys.calculateDebts.key => controller.calculateDebts()
+          case TuiKeys.quit.key           => controller.quit
+          case TuiKeys.undo.key           => controller.undo()
+          case TuiKeys.redo.key           => controller.redo()
+          case TuiKeys.newGroup.key       => controller.addGroup(tokens(1))
+          case TuiKeys.gotoGroup.key      => controller.gotoGroup(tokens(1))
+          case TuiKeys.setStrategy.key    => controller.setDebtStrategy(tokens(1))
+          case TuiKeys.addUserToGroup.key =>
+            tokens.drop(1)
+              .foreach(user_name => controller.addUserToGroup(user_name))
+          case TuiKeys.addExpense.key =>
+            controller.addExpenseToGroup(
+              tokens(1),
+              tokens(2),
+              tokens(3).toDouble,
+              date = Date(1, 1, 2000), // TODO: fix this
+              shares = tokens.lift(4)
+            )
+      case Failure(error) =>
+        print(s"${error.getMessage()}\n>")
+

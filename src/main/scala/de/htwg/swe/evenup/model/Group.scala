08f43a9bda69b5427fa2a4365aa6c7f4e4cf3fa6
jonaskromer <jonas.kromer27@gmail.com>
2025-12-04 20:32:37 +0100

    fix: the printing

--- src/main/scala/de/htwg/swe/evenup/model/Group.scala
+++ src/main/scala/de/htwg/swe/evenup/model/Group.scala
@@ -21,7 +21,7 @@ final case class Group(
     val members_string  = members.map(_.toString()).mkString(", ")
     val expenses_string = expenses.map(_.toString()).mkString("\n")
 
-    f"Users: $members_string\nExpenses:\n$expenses_string"
+    f"The group $name has the following users and expenses.\nUsers: $members_string\nExpenses:\n$expenses_string"
 
   def addMember(person: Person): Group =
     if (members.contains(person))

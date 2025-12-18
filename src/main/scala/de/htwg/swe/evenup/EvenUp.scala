7fb855c60727576200d15ae3a145de37afe75059
Bryan Wiest <bryan@wiestweb.com>
2025-12-18 18:17:48 +0100

    feat: integrate GUI into main with thread

--- src/main/scala/de/htwg/swe/evenup/EvenUp.scala
+++ src/main/scala/de/htwg/swe/evenup/EvenUp.scala
@@ -5,6 +5,7 @@ import de.htwg.swe.evenup.control.BaseControllerImpl.Controller
 
 import scala.io.StdIn.readLine
 import de.htwg.swe.evenup.view.tui.Tui
+import de.htwg.swe.evenup.view.gui.Gui
 import de.htwg.swe.evenup.model.StateComponent.BaseAppStateImpl.MainMenuState
 
 @main def main(): Unit =
@@ -12,6 +13,9 @@ import de.htwg.swe.evenup.model.StateComponent.BaseAppStateImpl.MainMenuState
   val app        = App(Nil, None, None, MainMenuState())
   val controller = new Controller(app)
   val tui        = new Tui(controller)
+  val gui        = new Gui(controller)
+
+  new Thread(() => gui.main(Array.empty)).start()
 
   var input = ""
 

a22ca77c6b1105ac8aee601334e67c7ce1e49229
Bryan Wiest <bryan@wiestweb.com>
2026-01-11 17:49:41 +0100

    fix: adding injection to main after revert

--- src/main/scala/de/htwg/swe/evenup/EvenUp.scala
+++ src/main/scala/de/htwg/swe/evenup/EvenUp.scala
@@ -1,24 +1,23 @@
 package de.htwg.swe.evenup
 
-import de.htwg.swe.evenup.model.AppComponent.BaseAppImpl.App
-import de.htwg.swe.evenup.control.BaseControllerImpl.Controller
-
 import scala.io.StdIn.readLine
+import com.google.inject.Guice
+
+import de.htwg.swe.evenup.control.IController
 import de.htwg.swe.evenup.view.tui.Tui
 import de.htwg.swe.evenup.view.gui.Gui
-import de.htwg.swe.evenup.model.StateComponent.BaseAppStateImpl.MainMenuState
+import de.htwg.swe.evenup.module.EvenUpModule
 
 @main def main(): Unit =
-
-  val app        = App(Nil, None, None, MainMenuState())
-  val controller = new Controller(app)
-  val tui        = new Tui(controller)
-  val gui        = new Gui(controller)
+  val injector = Guice.createInjector(new EvenUpModule())
+  
+  val controller = injector.getInstance(classOf[IController])
+  val tui        = injector.getInstance(classOf[Tui])
+  val gui        = injector.getInstance(classOf[Gui])
 
   new Thread(() => gui.main(Array.empty)).start()
 
   var input = ""
-
   while (input != "exit")
     input = readLine()
     tui.processInput(input)

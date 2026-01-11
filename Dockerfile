6377a2571e1f470ffdf6dcb039bd2a4aa6be91c3
jonaskromer <jonas.kromer27@gmail.com>
2026-01-11 14:31:06 +0100

    feat: base docker impl

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM sbtscala/scala-sbt:eclipse-temurin-jammy-22_36_1.10.0_3.4.2
+
+RUN apt update && \
+    apt install -y \
+    libxrender1 \
+    libxtst6 \
+    libxi6 \
+    libgl1-mesa-glx \
+    libgtk-3-0 \
+    libgl1-mesa-dri \
+    libgl1-mesa-dev \
+    libcanberra-gtk-module \
+    libcanberra-gtk3-module \
+    default-jdk
+
+RUN apt update && \
+    apt install -y \
+    xvfb \
+    x11vnc
+
+# Set environment variables for X11 forwarding
+ENV DISPLAY=host.docker.internal:0
+ENV LIBGL_ALWAYS_INDIRECT=true
+
+# Create a directory for X11 UNIX socket
+RUN mkdir -p /tmp/.X11-unix && chmod 1777 /tmp/.X11-unix
+
+WORKDIR /evenup
+ADD . /evenup
+
+RUN sbt assembly
+
+CMD ["java", "-jar", "target/scala-3.7.3/EvenUp.jar"]
--- src/main/scala/de/htwg/swe/evenup/EvenUp.scala
+++ src/main/scala/de/htwg/swe/evenup/EvenUp.scala
@@ -1,26 +1,24 @@
 package de.htwg.swe.evenup
 
-import de.htwg.swe.evenup.model.AppComponent.BaseAppImpl.App
-import de.htwg.swe.evenup.control.BaseControllerImpl.Controller
+import de.htwg.swe.evenup.view.tui.Tui
+import de.htwg.swe.evenup.view.gui.Gui
+import de.htwg.swe.evenup.control.IController
 import de.htwg.swe.evenup.modules.Default.given
 
 import scala.io.StdIn.readLine
-import de.htwg.swe.evenup.view.tui.Tui
-import de.htwg.swe.evenup.view.gui.Gui
-import de.htwg.swe.evenup.model.StateComponent.BaseAppStateImpl.MainMenuState
-import de.htwg.swe.evenup.model.AppComponent.IApp
-import de.htwg.swe.evenup.control.IController
 
-@main def main(): Unit =
 
+object EvenUp:
   val controller: IController = summon[IController]
   val tui        = new Tui(controller)
   val gui        = new Gui(controller)
 
-  new Thread(() => gui.main(Array.empty)).start()
+  def main(args: Array[String]): Unit =
+    new Thread(() => 
+      gui.main(Array.empty)).start()
 
-  var input = ""
+    var input: String = ""
 
-  while (input != "exit")
-    input = readLine()
-    tui.processInput(input)
+    while (input != "exit")
+      input = readLine()
+      tui.processInput(input)

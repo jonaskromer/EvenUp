2aaf5210cd7d61bd1b4e9f31776aa9c6aa8a9bc2
Bryan Wiest <bryan@wiestweb.com>
2026-01-17 13:09:39 +0100

    Revert "fix: corrected typo"
    This reverts commit bc2514ad9d4b3b27e14099975221ab8ab36cb86c.

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Starting EvenUp on Windows...
-docker compose -f docker-compose.yaml -f docker-compose.windows.yaml up --build
+docker compose -f docker-compose.yml -f docker-compose.windows.yml up --build
 pause
--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@ OS_TYPE="$(uname -s)"
 
 if [[ "$OS_TYPE" == "Darwin" ]]; then
     echo "Starting EvenUp on macOS..."
-    docker compose -f docker-compose.yaml -f docker-compose.macos.yaml up --build
+    docker compose -f docker-compose.yml -f docker-compose.macos.yml up --build
 elif [[ "$OS_TYPE" == "Linux" ]]; then
     echo "Starting EvenUp on Linux..."
-    docker compose -f docker-compose.yaml -f docker-compose.linux.yaml up --build
+    docker compose -f docker-compose.yml -f docker-compose.linux.yml up --build
 else
     echo "Unsupported OS: $OS_TYPE"
     exit 1
